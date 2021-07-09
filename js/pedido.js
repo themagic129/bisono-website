3f626281e48dec3dded9a2730d504d382d9f084e
Jose <jose@miemail.com>
2021-07-09 14:35:57 -0400

    img header

--- js/pedido.js
+++ js/pedido.js
@@ -2,7 +2,8 @@ const carro = new Carrito();
 const carrito = document.getElementById('carrito');
 const productos = document.getElementById('lista-productos');
 const listaProductos = document.querySelector('#lista-carrito tbody');
-
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+const procesarPedidoBtn = document.getElementById('procesar-pedido');
 
 cargarEventos();
 
@@ -15,5 +16,11 @@ function cargarEventos(){
     carrito.addEventListener('click', (e)=>{carro.eliminarProducto(e)});
 
     //Al vaciar carrito
- 
+    vaciarCarritoBtn.addEventListener('click', (e)=>{carro.vaciarCarrito(e)});
+
+    //Al cargar documento se muestra lo almacenado en LS
+    document.addEventListener('DOMContentLoaded', carro.leerLocalStorage());
+
+    //Enviar pedido a otra pagina
+    procesarPedidoBtn.addEventListener('click', (e)=>{carro.procesarPedido(e)});
 }
