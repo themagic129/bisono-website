69515b08138b18d5c8d4496d6e8f4b9684eab934
Jose <jose@miemail.com>
2021-07-13 11:31:03 -0400

    avances

---
+++ js/navbar.js
@@ -0,0 +1,40 @@
+
+
+$(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 20) {
+            $('.navbar').addClass('cambio');
+
+            
+}
+
+    else {
+       
+        $('.navbar').removeClass('cambio');
+        
+}
+});
+})
+
+
+$(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 20) {
+            $('.nav-link').addClass('nav-link-color-scroll');
+
+            
+}
+
+    else {
+       
+        $('.nav-link').removeClass('nav-link-color-scroll');
+        
+}
+});
+})
+
+
+
+
+
+
